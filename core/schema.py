783a4aba15d5e1a31df2a83186ae85cc2fe65228
PoojaGupta0 <poojagupt538@gmail.com>
2022-05-04 17:41:51 +0530

    added a graphql endpoint to create a new teacher and remove unwanted code from schema file

--- core/schema.py
+++ core/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from core.models import TeachersOfStudent
+from core.models import Teacher, TeachersOfStudent
 
 
 class StudentsOfTeachersType(DjangoObjectType):
@@ -9,11 +9,9 @@ class StudentsOfTeachersType(DjangoObjectType):
         model = TeachersOfStudent
 
 
-class Query(graphene.ObjectType):
-    all_student_of_teachers = graphene.List(StudentsOfTeachersType)
-
-    def resolve_all_student_of_teachers(self, info, **kwargs):
-        return TeachersOfStudent.objects.all().order_by('-id')
+class TeachersType(DjangoObjectType):
+    class Meta:
+        model = Teacher
 
 
 class AddOrRemoveStar(graphene.Mutation):
@@ -25,13 +23,29 @@ class AddOrRemoveStar(graphene.Mutation):
     def mutate(self, info, id):
         students_of_teacher = TeachersOfStudent.objects.get(pk=id)
         if students_of_teacher is not None:
-            students_of_teacher.is_stared = False if students_of_teacher.is_stared == True else True
+            students_of_teacher.is_stared = (
+                False if students_of_teacher.is_stared == True else True
+            )
             students_of_teacher.save()
         return AddOrRemoveStar(students_of_teacher=students_of_teacher)
 
 
+class CreateTeacher(graphene.Mutation):
+    __doc__ = "This class is used to create a new teacher"
+
+    class Arguments:
+        name = graphene.String()
+
+    teacher = graphene.Field(TeachersType)
+
+    def mutate(self, info, name):
+        teacher = Teacher.objects.create(name=name)
+        return CreateTeacher(teacher=teacher)
+
+
 class Mutation(graphene.ObjectType):
     add_or_remove_star = AddOrRemoveStar.Field()
+    create_teacher = CreateTeacher.Field()
 
 
 schema = graphene.Schema(mutation=Mutation)
