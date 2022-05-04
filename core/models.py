49f2fade071e03c3530c378f04daaabf35ab9057
PoojaGupta0 <poojagupt538@gmail.com>
2022-05-04 16:32:25 +0530

    added student and teacher model and their migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50, verbose_name="student_name")
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50, verbose_name="teacher_name")
+
+
+class TeachersOfStudent(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    is_stared = models.BooleanField(default=False)
